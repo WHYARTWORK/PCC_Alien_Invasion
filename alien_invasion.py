b6931e34c014e29b2e8fccc1568d856b4cdc5080
WHY_ARTWORK <whyartwork@gmail.com>
2019-06-24 21:26:33 -0700

    Try it yourself 12-2. Display a second image on screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from hollowknight import TheKnight
+from shiptwo import ShipTwo
 
 def run_game():
     # Initialize pygame, settings, and screen object.
@@ -14,10 +16,12 @@ def run_game():
 
     #Make a ship.
     ship = Ship(screen)
-    
+    knight = TheKnight(screen)
+    shiptwo = ShipTwo(screen)
+
     #Start the main loop for the game.
     while True:
         gf.check_events()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, knight, shiptwo)
 
 run_game()
