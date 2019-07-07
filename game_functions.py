1340feea90288486a046523b685f7ccb0cfe2655
WHY_ARTWORK <whyartwork@gmail.com>
2019-07-06 17:26:38 -0700

    make an alien class

--- game_functions.py
+++ game_functions.py
@@ -22,6 +22,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def check_keyup_events(event, ship):
     """Respond to key releases"""
@@ -30,7 +32,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on the screen and flip to the new screen."""
     #Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -40,6 +42,8 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
+
+    alien.blitme()
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
